efbbd3131d67901d92255bc668c28d681e6549b3
Andre Pato <anpato1994@gmail.com>
2020-11-26 16:03:54 -0500

    add setup

---
+++ setup.js
@@ -0,0 +1,21 @@
+const { readdirSync } = require('fs')
+const { exec } = require('child_process')
+const { promisify } = require('util')
+const execute = promisify(exec)
+const base_dir = readdirSync(__dirname).find((f) => f.includes('start'))
+console.log('Hi')
+const working_dirs = readdirSync(`${__dirname}/${base_dir}`)
+let starter = `${__dirname}/${base_dir}`
+Promise.all(
+  working_dirs.map(async (dir) => {
+    try {
+      await execute(
+        ` cd ${starter}/${dir} && sequelize db:create && sequelize db:migrate && sequelize db:seed:all && cd ../`
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  })
+)
+
+// module.exports = stringer
